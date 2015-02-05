f7de0444d12d29cfb6a75358772211740c2d8f7c
matejpetrov <petrov_matej@yahoo.com>
2015-02-05 16:46:58 +0100

    AJAX username check start

---
+++ app/assets/javascripts/username_email_check_AJAX.js
@@ -0,0 +1,12 @@
+
+jQuery(document).ready(function() { 
+
+
+	$("#session_username").on("focusout", function(){
+
+		alert("Focus out");
+
+	});
+
+
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( delete_user.js )
 Rails.application.config.assets.precompile += %w( ajax_functions.js )
+Rails.application.config.assets.precompile += %w( username_email_check_AJAX.js )
